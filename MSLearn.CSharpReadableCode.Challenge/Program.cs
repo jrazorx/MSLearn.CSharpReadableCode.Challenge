79b435a25dd1874d523d115644fe91c654c9a7a5
jra <julien.gault.50@gmail.com>
2024-02-20 16:48:12 +0100

    Add project files.

---
+++ MSLearn.CSharpReadableCode.Challenge/Program.cs
@@ -0,0 +1,13 @@
+﻿string str = "The quick brown fox jumps over the lazy dog.";
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
+// Reverse the chars
+Array.Reverse(charMessage);
+int x = 0;
+// count the o's
+foreach (char i in charMessage) { if (i == 'o') { x++; } }
+// convert it back to a string
+string new_message = new String(charMessage);
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
